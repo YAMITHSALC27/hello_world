fa01b4c84313058c599e32155326e194d9815efc
YAMITHSALC27 <salcedoyamith@gmail.com>
2024-09-11 16:48:55 -0500

    se agrego un archivo prueba2.py

--- prueba.py
+++ prueba.py
@@ -1,6 +1,16 @@
 import time
 
-print("hola mundols")
-print("segundo renglon\n")
-time.sleep(10)
+print("hola") # esta linea imprime en consola la pralabra hola
+print("hola mundo\n")
+time.sleep(3)
 print("cachirrie")
+
+print(type("hola")) # la funcion type me dice que tipo de dato es lo que esta en los parentesis
+print(type(4))
+print(type(5.5))
+
+"""
+dnafninaiaondinadiniw
+anfnaanidninfaklfkl
+nafjanfdkwandiawlnd
+"""
